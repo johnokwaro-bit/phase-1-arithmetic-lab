4c37c2afd221516f85eecee8f7c96655fb795af6
John Okwaro <john.okwaro@moringaschool.com>
2022-08-17 22:04:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;   
+    
+
+let random = Math.ceil(Math.random());
+
+
+let num3 = 10;
+let num4 = 6;
+let mod= num3 % num4;
+
+
+let max;
+max = Math.max(1,12, 10, 15, 20);
+
+
+
+
+
+
+
+
+
